9606a72d2c27e0baf19207b1135decf53afc2497
Natasha <admin@natashaosborne.com>
2022-02-18 14:14:29 -0700

    hide/show password field

--- src/app/Home.js
+++ src/app/Home.js
@@ -4,18 +4,20 @@ import {
   Route,
 } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { TextField, Button, Grid } from '@mui/material';
+import { TextField, Button, Grid, InputAdornment, IconButton } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 import Nutrition from './Nutrition.js';
 import BottomNavigation from './BottomNavigation';
 import Header from './Header';
 import ResponsiveDrawer from './ResponsiveDrawer';
-
 import { useLoginMutation } from './services/fitnessApi';
 
 export default function Home() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, togglePasswordVisibility] = useState(false);
 
   const [loginUser] = useLoginMutation({fixedCacheKey: 'user-auth'});
   const user = useSelector(({user}) => user);
@@ -42,16 +44,26 @@ export default function Home() {
         <div className="Main">
           <form className="login-form">
             <TextField
-              id="outlined-textarea"
               label="Enter your Email"
               placeholder="Email"
               onChange={({target}) => setEmail(target.value)}
             />
             <TextField
-              id="outlined-textarea"
+              type={showPassword ? 'text' : 'password'}
               label="Enter your Password"
               placeholder="Password"
               onChange={({target}) => setPassword(target.value)}
+              InputProps={{endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => togglePasswordVisibility((visibility) => !visibility)}
+                    edge="end"
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              )}}
             />
             <Button variant="contained" onClick={(e) => login(e)}>Login</Button>
           </form>
@@ -65,13 +77,13 @@ export default function Home() {
               <Route path="/" element={<div></div>}/>
             </Routes>
           </div>
-          <div className="Footer">
-            <footer>
-              <BottomNavigation />
-            </footer>
-          </div>
         </>
       )}
+      <div className="Footer">
+        <footer>
+          <BottomNavigation />
+        </footer>
+      </div>
     </Grid>
   );
 }
