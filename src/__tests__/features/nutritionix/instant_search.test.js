0f1c7e7f0764e999a630dda5a0fcec6aed3c2fbc
Natasha <admin@natashaosborne.com>
2022-02-22 16:50:06 -0700

    use var for searchText

--- src/__tests__/features/nutritionix/instant_search.test.js
+++ src/__tests__/features/nutritionix/instant_search.test.js
@@ -9,14 +9,14 @@ const user = {
 const token = 'token';
 const authenticatedUser = {...user, token};
 const expectedResults = _.flatten([...appleSearchResults.common, ...appleSearchResults.branded]);
-
+const searchText = 'apple';
 
 describe('Instant search', () => {
   const loginResponse = {
     data: authenticatedUser
   };
   const instantSearchResponse = {
-    config: {params: 'apple'},
+    config: {params: searchText},
     data: appleSearchResults
   };
 
@@ -26,12 +26,11 @@ describe('Instant search', () => {
   it('displays the search results', async () => {
     const initialEntries = ['/nutrition'];
     const {container} = mountApp({user: authenticatedUser}, {initialEntries});
-    const searchText = 'apple';
 
     expect(container).toHaveTextContent('Nutrition Page');
     fillIn(screen, 'Search for Foods').with(searchText);
 
-    await screen.findAllByText('apple');
+    await screen.findAllByText(searchText);
 
     expectedResults.map(({food_name}) => {
       expect(container).toHaveTextContent(food_name);
