b7d6ca3b46a3be8fd4553dc68fd5d418a049c87c
Natasha <admin@natashaosborne.com>
2022-02-23 03:48:05 -0700

    update imports

--- src/__mocks__/index.js
+++ src/__mocks__/index.js
@@ -1,5 +1,5 @@
-import FitnessAPI from './FitnessAPI';
-import NutritionixAPI from './NutritionixAPI';
+import FitnessAPI from '__mocks__/FitnessAPI';
+import NutritionixAPI from '__mocks__/NutritionixAPI';
 
 export const mockFitnessAPI = FitnessAPI;
 export const mockNutritionixAPI = NutritionixAPI;
